9d22d6f7308e3b0c8ca377b898c718dc170c1b65
Jacek Pierzchlewski <jap@es.aau.dk>
2014-05-14 23:04:33 +0200

    RxCS paths are added to conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,15 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../rxcs-engine/'))
+sys.path.insert(0, os.path.abspath('../rxcs-engine/sig'))
+sys.path.insert(0, os.path.abspath('../rxcs-engine/acq'))
+sys.path.insert(0, os.path.abspath('../rxcs-engine/cs'))
+sys.path.insert(0, os.path.abspath('../rxcs-engine/ana'))
+sys.path.insert(0, os.path.abspath('../rxcs-engine/work_frame1'))
+
 
 # -- General configuration -----------------------------------------------------
 
