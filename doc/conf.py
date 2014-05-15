622a4533d7f7d611d6c7091cacd7ff2a4007165f
jap@es.aau.dk <jap@es.aau.dk>
2014-05-15 13:05:40 +0000

    Test modules added to documentation path

--- doc/conf.py
+++ doc/conf.py
@@ -18,6 +18,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../test/'))
 sys.path.insert(0, os.path.abspath('../rxcs/'))
 sys.path.insert(0, os.path.abspath('../rxcs/sig'))
 sys.path.insert(0, os.path.abspath('../rxcs/acq'))
