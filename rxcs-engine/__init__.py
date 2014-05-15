e916309a4f8e996599b12beaecc53d424d1498a1
jap@es.aau.dk <jap@es.aau.dk>
2014-05-15 10:21:55 +0000

    __init.py__ added to rxcs-engine/ directory

---
+++ rxcs-engine/__init__.py
@@ -0,0 +1,22 @@
+
+# Import signal generators
+import sig
+
+# Import acquisition system
+import acq
+
+# Import compressed sensing modules
+import cs
+
+# Import system analysis modules
+import ana
+
+# Import main frame1
+from work_frame1 import work_frame1
+
+# Import console printing function
+import console
+
+
+
+
