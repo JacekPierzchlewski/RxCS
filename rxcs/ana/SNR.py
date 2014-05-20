c1a5b5e40b18ea3f74a9f4d5b246d169979eaad6
jap@es.aau.dk <jap@es.aau.dk>
2014-05-20 15:12:37 +0000

    SNR evaluation of the reconstructed signal initialized

--- rxcs/ana/SNR.py
+++ rxcs/ana/SNR.py
@@ -1,29 +1,53 @@
-from __future__import division
+from __future__ import division
 import numpy as np
 
-def main(dSig, dSigRecon, dAna):
+def main(dSigOrig, dSigRecon, dAna):
 
-  # Get the original unnoisy signals, the number of signal and their length
-  mSig_orig = dSigOrig['mSigNN']
-  (nSigs, nL_orig) = mSig_orig.shape()
+    ## Get the original unnoisy signals, the number of signal and their length
+    mSig_orig = dSigOrig['mSigNN']
+    (iSigs_orig, iSiz_orig) = mSig_orig.shape
 
-  # Get the reconstructed signals and their length
-  mSig_recon = dSigRecon['mSig']
-  (_, nL_recon) = mSig_orig.shape()
+    # Get the reconstructed signals and their length
+    mSig_recon = dSigRecon['mSig']
+    (iSigs_recon, iSiz_recon) = mSig_orig.shape
 
-  # -------------------------------------------------------------------
+    # Check if the original and the reconstructed signals have the same length
+    strErr = 'The original and reconstructed signals must have the same length'
+    if iSiz_orig != iSiz_recon:
+        raise ValueError(strErr)
 
-  # Compute the noise signals
-  mNoise = np.abs(mSig_orig - mSig_recon)
+    # Check if there is the same number of original and reconstructed signals
+    strErr = 'There are more original signals than reconstructed signals!'
+    if iSigs_orig > iSigs_recon:
+        raise ValueError(strErr)
+    strErr = 'There are more reconstructed signals than original signals!'
+    if iSigs_recon > iSigs_orig:
+        raise ValueError(strErr)
 
-  # Compute the power of noise
-  vNoiseP = (np.sum(mNoise*mNoise,axis=1) / nL_orig).reshape(nSigs,1)
+    # -------------------------------------------------------------------
 
-  # Compute the power of orignal signals
-  vSigP = (np.sum(mSig_orig*mSig_orig,axis=1) / nL_orig).reshape(nSigs,1)
+    # Compute the noise
+    mNoise = np.abs(mSig_orig - mSig_recon)
+    (_, iSizNoise) = mNoise.shape  # Size of the noise
 
-  # Compute the SNR for every reconstructed signal and add it to the dictionary
-  # with system analysis results
-  vSNR = 20*log10(vSigP/vNoiseP)
-  dAna['vSNR'] = vSNR
-  return dAna
+    # Compute the power of noise
+    vNoiseP = (np.sum(mNoise * mNoise,axis=1) / iSizNoise)
+
+    # Compute the power of orignal signals
+    vSigP = (np.sum(mSig_orig * mSig_orig,axis=1) / iSiz_orig)
+
+    # Compute the SNR for every reconstructed signal and the average SNR
+    vSNR = 10 * np.log10(vSigP / vNoiseP)
+    iSNR = vSNR.mean()
+
+    # Compute the
+    # -------------------------------------------------------------------
+    # Add the vector with computed SNR to the dictionary with system
+    # analysis results
+    dAna['vSNR'] = vSNR
+
+    # Add the average SNR to the dictionary with system analysis results
+    dAna['iSNR'] = iSNR
+
+    # -------------------------------------------------------------------
+    return dAna
