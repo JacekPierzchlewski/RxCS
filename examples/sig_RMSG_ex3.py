b975c861d232fc873c00b12e1ae57367a08d93f1
jap@es.aau.dk <jap@es.aau.dk>
2014-05-21 11:09:57 +0000

    Version 1.0 of the example #3 of using the Random Multitone Signal Generator is released

--- examples/sig_RMSG_ex3.py
+++ examples/sig_RMSG_ex3.py
@@ -1,3 +1,42 @@
+"""
+This script is an example of how to use the Random Multitone Signal
+Generator module. |br|
+
+In this example 4 random multitone signal is generated. |br|
+
+Time of the signal is 10 us, the signal representation sampling frequency is
+100 MHz. The highest possible frequency of a tone in the signal is 40 MHz,
+the signal spectrum resolution is 100 kHz. |br|
+
+The signal contains 11 tones with specified frequencies.
+Frequencies of the first 6 tones are specified, these frequencies are
+1kHz, 2kHz, 3kHz, 4kHz, 5kHz and 6kHz.
+The amplitudes of the first 6 tones are specified, all are equal to 1.
+Frequencies of the next 5 tones are specified, these frequencies are
+22kHz, 23kHz, 24kHz, 25kHz and 26kHz.
+The amplitudes of these 5 tones are not specified,
+The phases of all 11 tones are randomly chosen by the generator. |br|
+
+There are no additional random tones in the signal. |br|
+
+The power of the signals is adjusted to 1 W. |br|
+
+The noise is not added to the signal. |br|
+
+After the generation, the power of signals at different frequencies
+is analyzed using the Welch's method.
+
+*Author*:
+    Jacek Pierzchlewski, Aalborg University, Denmark. <jap@es.aau.dk>
+
+*Version*:
+    0.1  | 15-MAY-2014 : * Initial version. |br|
+    0.2  | 21-MAY-2014 : * Docstrings added and PEP8 adjustments. |br|
+    1.0  | 21-MAY-2014 : * Version 1.0 released. |br|
+
+*License*:
+    BSD 2-Clause
+"""
 from __future__ import division
 import rxcs
 import numpy as np
