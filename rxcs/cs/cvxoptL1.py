b661ff1d55cd4de91e730ac436141e910d527349
jap@es.aau.dk <jap@es.aau.dk>
2014-06-12 11:47:15 +0000

    License info updated. RxCS now uses two licenses. Most of the code is covered with BSD 2-clause, soem parts are covered with GNU GPL V3

--- rxcs/cs/cvxoptL1.py
+++ rxcs/cs/cvxoptL1.py
@@ -1,6 +1,22 @@
 """
 This a L1-optimization signal reconstruction module. |br|
 
+Copyright (C) <2014>  Jacek Pierzchlewski
+              <2014>  Martin S. Andersen, Joachim Dahl, and Lieven Vandenberghe
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 *Authors*:
     Jacek Pierzchlewski, Aalborg University, Denmark. <jap@es.aau.dk>
 
