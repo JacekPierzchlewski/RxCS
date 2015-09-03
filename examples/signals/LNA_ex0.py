9e6d7c41b7e9a122cb3a14cbb2afefc65155b83e
Jacek <jap@es.aau.dk>
2015-09-03 13:53:31 +0200

    Improvements in example #1 for low-nosie signal amplifier

--- examples/signals/LNA_ex0.py
+++ examples/signals/LNA_ex0.py
@@ -8,12 +8,13 @@ model. |br|
 
 *Version*:
     1,0  | 02-SEP-2015  : * Version 1.0 released. |br|
+    1,1  | 03-SEP-2015  : * Improvements in signals plotting. |br|
 
 *License*:
     BSD 2-Clause
 """
 
-
+from __future__ import division
 import rxcs
 import matplotlib.pyplot as plt
 import numpy as np
@@ -29,12 +30,12 @@ def _LNA_ex0():
     gen.fR = 1e9      # The signal representation sampling frequency is 1 GHz
     gen.fRes = 1e6    # The signal spectrum resolution is 1 MHz
 
-    gen.vFrqs = np.array([ 1e6 ])   # There is one tone: 1 MHz cosine... 
+    gen.vFrqs = np.array([ 10e6 ])  # There is one tone: 10 MHz cosine... 
     gen.vAmps = np.array([ 1 ])     # ...with amplitude = 1/...
     gen.vPhs =  np.array([ 0 ])     # and no phase shift
 
     # Settings for the low-noise amplifier
-    lna.vCoef = np.array([1.2, 0.1, -0.5])    # Vector with LNA coefficients
+    lna.vCoef = np.array([1.0, 0.0, -0.5, 0.0, -0.43])    # Vector with LNA coefficients
 
     # Run the generator and get the output    
     gen.run()    
@@ -52,10 +53,11 @@ def _LNA_ex0():
     hFig1 = plt.figure(1)
     hSubPlot1 = hFig1.add_subplot(111)
     hSubPlot1.grid(True)
-    hSubPlot1.set_title('Random multitone signal in the time domain')
+    hSubPlot1.set_title('Signal in the time domain')
     hSubPlot1.set_xlabel('Frequency [Hz]')
-    hSubPlot1.plot(vTSig, vOrgSig, 'b-')
-    hSubPlot1.plot(vTSig, vLNASig, 'k-')    
+    hSubPlot1.plot(vTSig, vOrgSig, 'b-', label='original')
+    hSubPlot1.plot(vTSig, vLNASig, 'k-', label='after the LNA')
+    hSubPlot1.legend()    
     hSubPlot1.set_ylim(-3.0, 3.0)
 
     # -----------------------------------------------------------------
@@ -64,27 +66,47 @@ def _LNA_ex0():
     # Analyze spectrum of the signals
     fFFTR =  gen.fFFTR     # Get the signal FFT frequency resolution
 
-    (vFFTOrg, vFOrg) = _fftAnalysis(vOrgSig, fFFTR)     # The original signal
-    #(vFFTLNA, vFLNA) = _fftAnalysis(vLNASig, fFFTR)     # The signal after LNA
+    (vFFTOrg, vFOrg) = _fftAnalysis(vOrgSig, fFFTR, minamp=1e-3)  # The original signal
+    (vFFTLNA, vFLNA) = _fftAnalysis(vLNASig, fFFTR, minamp=1e-3)  # The signal after the LNA
 
     hFig2 = plt.figure(2)
     hSubPlot1 = hFig2.add_subplot(111)
     hSubPlot1.grid(True)
-    hSubPlot1.set_title('Spectrum of a random multitone signal')
+    hSubPlot1.set_title('Signals in the frequency domain')
     hSubPlot1.set_xlabel('Frequency [Hz]')
-    (markerline, stemlines, baseline) = hSubPlot1.stem(vFOrg, vFFTOrg,
-                                                       linefmt='b-',
-                                                       markerfmt='bo',
-                                                       basefmt='.')
-    hSubPlot1.set_xlim(-1*1e3, 10*1e6)
+    
+    # original signal
+    (m, s, _) = hSubPlot1.stem(vFOrg, vFFTOrg, basefmt='.', label='original')
+    plt.setp(s, color='b', linewidth=2.0)
+    plt.setp(m, color='b', markersize=10.0)
+
+    # signal after the LNA
+    (m, s, _) = hSubPlot1.stem(vFLNA, vFFTLNA, basefmt='.', label='after the LNA')
+    plt.setp(s, color='k', linewidth=2.0)
+    plt.setp(m, color='k', markersize=10.0)
+   
+    hSubPlot1.set_xlim(-1e6, 100*1e6)
     hSubPlot1.set_ylim(-0.1, 1.5)
-    plt.setp(stemlines, color='b', linewidth=2.0)
-    plt.setp(markerline, color='b', markersize=10.0)
+    hSubPlot1.legend()    
 
     # -----------------------------------------------------------------
     plt.show(block=True)
 
-def _fftAnalysis(vSig, fFFTR):
+def _fftAnalysis(vSig, fFFTR, minamp=0.0):
+    """
+    This function analyse the signal in the frequency domain.
+    It return the positive half of the signal spectrum.
+
+    Args:
+        vSig (1D Numpy array):  signal to be analysed
+        fFFTR: (float):         FFT frequency resolution of the signal
+        minamp: (float):        minimum amplitude which should be included in
+                                the spectrum plot [default = 0.0  - include all]
+
+    Returns:
+       vFFTa_half (1D Numpy array):  amplitudes of tones in the spectrum
+       vF_half (1D Numpy array):     frequencies of tones in the spectrum
+    """
     
     # FFT analysis
     vFFT = np.fft.fft(vSig)    
@@ -100,6 +122,11 @@ def _fftAnalysis(vSig, fFFTR):
 
     # Create a vector with frequencies of the signal spectrum
     vF_half = fFFTR * np.arange(iS/2)
+
+    # Take only the important amplitudes
+    if minamp > 0.0:
+        vF_half = vF_half[vFFTa_half >= minamp] 
+        vFFTa_half = vFFTa_half[vFFTa_half >= minamp]
     
     return (vFFTa_half, vF_half)
 
